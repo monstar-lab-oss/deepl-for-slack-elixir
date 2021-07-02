c532f94098ef1c5f28d3388d0a1ec8bbcb347d3b
Milan Vit <milanvit@milanvit.net>
2021-07-02 17:26:46 +0900

    ✅ Add failing test for the signature verification

--- test/deep_thought_web/plugs/verify_signature_test.exs
+++ test/deep_thought_web/plugs/verify_signature_test.exs
@@ -15,10 +15,40 @@ defmodule DeepThoughtWeb.Plugs.VerifySignatureTest do
     {:ok,
      conn:
        conn(:post, "/slack/events", body)
-       |> Plug.Conn.put_private(:raw_body, body)}
+       |> Plug.Conn.put_private(:raw_body, body)
+       |> put_req_header("x-slack-request-timestamp", "1625202589")}
   end
 
   test "init/1 stores the signing secret" do
     assert "secret" == VerifySignature.init("secret")
+  end
+
+  test "request with valid signature is not halted", %{conn: conn} do
+    conn =
+      conn
+      |> put_req_header(
+        "x-slack-signature",
+        "v0=27ab08e51bb55b93f4cb2c749193ed0ea90986020a8fe2f0aa3972d480b9a715"
+      )
+      |> VerifySignature.call("secret")
+
+    refute conn.halted
+  end
+
+  test "request with an invalid signature is halted", %{conn: conn} do
+    conn =
+      conn
+      |> put_req_header("x-slack-signature", "invalid")
+      |> VerifySignature.call("secret")
+
+    assert conn.status == 401
+    assert conn.halted
+  end
+
+  test "request with a missing signature is halted", %{conn: conn} do
+    conn = VerifySignature.call(conn, "secret")
+
+    assert conn.status == 401
+    assert conn.halted
   end
 end
