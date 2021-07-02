32ae9b7488b3aecf5882d74fe61df95d1903a615
Milan Vit <milanvit@milanvit.net>
2021-07-02 13:01:08 +0900

    🔧 Add support for fetching env variables

--- config/prod.exs
+++ config/prod.exs
@@ -56,4 +56,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
+import_config "config.secret.exs"
 import_config "prod.secret.exs"
