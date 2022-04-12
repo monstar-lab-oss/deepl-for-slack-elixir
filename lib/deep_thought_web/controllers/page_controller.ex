79a808a4acfe4f37cdeb4d765c8b3b29b72fb267
Milan Vit <milanvit@milanvit.net>
2022-04-12 10:23:31 +0900

    🚨 Add missing spec

--- lib/deep_thought_web/controllers/page_controller.ex
+++ lib/deep_thought_web/controllers/page_controller.ex
@@ -1,6 +1,7 @@
 defmodule DeepThoughtWeb.PageController do
   use DeepThoughtWeb, :controller
 
+  @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     render(conn, "index.html")
   end
